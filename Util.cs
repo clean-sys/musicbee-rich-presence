9195ffec943098d3c563493b29a0d8f1f45cd7ff
cleanin <49769124+cleanin@users.noreply.github.com>
2021-01-31 21:47:15 -0500

    Cloned from Gitlab

---
+++ Util.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Net.Http;
+using MusicBeePlugin;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+
+namespace Utils
+{
+    public class Utility
+    {
+        [DllImport("kernel32.dll")]
+        private static extern Int32 WideCharToMultiByte(UInt32 CodePage, UInt32 dwFlags, [MarshalAs(UnmanagedType.LPWStr)] String lpWideCharStr, Int32 cchWideChar, [Out, MarshalAs(UnmanagedType.LPStr)] StringBuilder lpMultiByteStr, Int32 cbMultiByte, IntPtr lpDefaultChar, IntPtr lpUsedDefaultChar);
+
+        public static string Utf16ToUtf8(string utf16String)
+        {
+            Int32 iNewDataLen = WideCharToMultiByte(Convert.ToUInt32(Encoding.UTF8.CodePage), 0, utf16String, utf16String.Length, null, 0, IntPtr.Zero, IntPtr.Zero);
+            if (iNewDataLen > 1)
+            {
+                StringBuilder utf8String = new StringBuilder(iNewDataLen);
+                WideCharToMultiByte(Convert.ToUInt32(Encoding.UTF8.CodePage), 0, utf16String, -1, utf8String, utf8String.Capacity, IntPtr.Zero, IntPtr.Zero);
+
+                return utf8String.ToString();
+            }
+            else
+            {
+                return String.Empty;
+            }
+        }
+    }
+
+    public class Discord
+    {
+        public static HttpContent GetAssetList()
+        {
+            var response = Plugin.http_client.GetAsync($"https://discordapp.com/api/oauth2/applications/{Plugin.discord_id}/assets", HttpCompletionOption.ResponseContentRead);
+
+            return response.Result.Content;
+        }
+
+        public async static Task UploadAsset(string image_type, string album_name, string image_data)
+        {
+            string payload = JsonConvert.SerializeObject(new
+            {
+                name = album_name,
+                type = image_type,
+                image = image_data,
+            });
+
+            var content = new StringContent(payload, Encoding.UTF8, "application/json");
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+            var response = await Plugin.http_client.PostAsync($"https://discordapp.com/api/oauth2/applications/{Plugin.discord_id}/assets", content);
+
+            var response_content = await response.Content.ReadAsStringAsync();
+        }
+    }
+}
---
+++ mb_DiscordRichPresence.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Net.Http;
+using DiscordInterface;
+using Utils;
+using System.Net.Http.Headers;
+
+namespace MusicBeePlugin
+{
+    public partial class Plugin
+    {
+        private MusicBeeApiInterface mbApiInterface;
+
+        private PluginInfo about = new PluginInfo();
+
+        public static readonly HttpClient http_client = new HttpClient();
+
+        public static string discord_id = "Your Discord APP ID";
+
+        public static DiscordRPC.RichPresence rpc_presence = new DiscordRPC.RichPresence();
+
+        public PluginInfo Initialise(IntPtr apiInterfacePtr)
+        {
+            mbApiInterface = new MusicBeeApiInterface();
+            mbApiInterface.Initialise(apiInterfacePtr);
+            about.PluginInfoVersion = PluginInfoVersion;
+            about.Name = "Discord Rich Presence";
+            about.Description = "A Richer Rich Presence for Musicbee.";
+            about.Author = "@cleaninfla";
+            about.TargetApplication = "";   
+            about.Type = PluginType.General;
+            about.VersionMajor = 1;  
+            about.VersionMinor = 0;
+            about.Revision = 01; 
+            about.ReceiveNotifications = (ReceiveNotificationFlags.PlayerEvents | ReceiveNotificationFlags.TagEvents);
+            about.ConfigurationPanelHeight = 0;
+
+            http_client.DefaultRequestHeaders.Add("Authorization", "Your Authorization Key");
+
+            InitialiseDiscord();
+
+            return about;
+        }
+
+        private void InitialiseDiscord()
+        {
+            DiscordRPC.DiscordEventHandlers handlers = new DiscordRPC.DiscordEventHandlers();
+            handlers.readyCallback = HandleReadyCallback;
+            handlers.errorCallback = HandleErrorCallback;
+            handlers.disconnectedCallback = HandleDisconnectedCallback;
+            DiscordRPC.Initialize(discord_id, ref handlers, true, null);
+        }
+
+        private void HandleReadyCallback() { }
+        private void HandleErrorCallback(int errorCode, string message) { }
+        private void HandleDisconnectedCallback(int errorCode, string message) { }
+
+        private async void ProcessArtwork(string album)
+        {
+            string asset_list = await Discord.GetAssetList().ReadAsStringAsync();
+
+            string artwork_data = mbApiInterface.NowPlaying_GetArtwork();
+
+            string data_uri = "data:image/png;base64," + artwork_data;
+
+            if (asset_list.Contains(album))
+            {
+                rpc_presence.largeImageKey = album;
+                return;
+            }
+            else
+            {
+                rpc_presence.largeImageKey = "temp_uploading";
+                await Discord.UploadAsset("1", album, data_uri);
+                rpc_presence.largeImageKey = album;
+                return;
+            }
+        }
+
+        private void UpdatePresence(string artist, string track, string album, string duration, Boolean playing, int position, float volume, bool handle_artworks = false)
+        {
+            track = Utility.Utf16ToUtf8(track + " ");
+			artist = Utility.Utf16ToUtf8("by " + artist);
+
+            rpc_presence.state = artist.Substring(0, artist.Length - 1);
+            rpc_presence.details = track.Substring(0, track.Length - 1);
+
+            string to_largeText = album + " " + artist;
+
+            rpc_presence.largeImageText = to_largeText.Substring(0, to_largeText.Length - 1);
+
+			char[] albumArray = album.ToCharArray();
+
+            for (int i = 0; i < album.Length; i++)
+            {
+                if (album[i] == ' ' || album[i] == ':' || album[i] == '(' || album[i] == ')') albumArray[i] = '_'; // Discord doesn't like these chars in asset names.
+
+				else albumArray[i] = album[i];
+			}
+
+			string new_album = new String(albumArray).ToLower();
+
+            if (handle_artworks)
+                ProcessArtwork(new_album);
+
+            long now = (long)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+
+            if (playing)
+            {
+                rpc_presence.startTimestamp = now - position;
+
+                string[] durations = duration.Split(':');
+
+                long end = System.Convert.ToInt64(durations[0]) * 60 + System.Convert.ToInt64(durations[1]);
+
+                rpc_presence.endTimestamp = rpc_presence.startTimestamp + end;
+
+                rpc_presence.smallImageKey = "playing";
+                rpc_presence.smallImageText = "Playing";
+            }
+
+            else
+            {
+                rpc_presence.endTimestamp = 0;
+                rpc_presence.startTimestamp = 0;
+                rpc_presence.smallImageKey = "paused";
+                rpc_presence.smallImageText = "Paused";
+            }
+
+            DiscordRPC.UpdatePresence(ref rpc_presence);
+        }
+
+        public bool Configure(IntPtr panelHandle)
+        {
+            string dataPath = mbApiInterface.Setting_GetPersistentStoragePath();
+            
+            if (panelHandle != IntPtr.Zero)
+            {
+                Panel configPanel = (Panel)Panel.FromHandle(panelHandle);
+                Label prompt = new Label();
+                prompt.AutoSize = true;
+                prompt.Location = new Point(0, 0);
+                prompt.Text = "prompt:";
+                TextBox textBox = new TextBox();
+                textBox.Bounds = new Rectangle(60, 0, 100, textBox.Height);
+                configPanel.Controls.AddRange(new Control[] { prompt, textBox });
+            }
+            return false;
+        }
+
+        public void SaveSettings()
+        {
+            string dataPath = mbApiInterface.Setting_GetPersistentStoragePath();
+        }
+
+        public void Close(PluginCloseReason reason)
+        {
+            DiscordRPC.Shutdown();
+        }
+
+        public void Uninstall()
+        {
+        }
+
+        public void ReceiveNotification(string sourceFileUrl, NotificationType type)
+        {
+            string artist = mbApiInterface.NowPlaying_GetFileTag(MetaDataType.Artist);
+            string track_title = mbApiInterface.NowPlaying_GetFileTag(MetaDataType.TrackTitle);
+			string album = mbApiInterface.NowPlaying_GetFileTag(MetaDataType.Album);
+            string duration = mbApiInterface.NowPlaying_GetFileProperty(FilePropertyType.Duration);
+            float volume = mbApiInterface.Player_GetVolume();
+            int position = mbApiInterface.Player_GetPosition();
+
+            if (string.IsNullOrEmpty(artist)) { artist = "[artist empty]"; }
+
+            switch (type)
+            {
+                case NotificationType.PlayStateChanged:
+                    switch (mbApiInterface.Player_GetPlayState())
+                    {
+                        case PlayState.Playing:
+                            UpdatePresence(artist, track_title, album, duration, true, position / 1000, volume);
+                            break;
+                        case PlayState.Paused:
+                            UpdatePresence(artist, track_title, album, duration, false, 0, volume);
+                            break;
+                    }
+                    break;
+                case NotificationType.TrackChanged:
+                    UpdatePresence(artist, track_title, album, duration, true, 0, volume, true);
+                    break;
+            }
+        }
+   }
+}
